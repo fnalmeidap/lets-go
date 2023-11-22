f6bc4ed8e320e6af2bebdf429a3aebe42e89c70e
Felipe Nunes <fnap@cin.ufpe.br>
2023-11-22 16:21:54 -0300

    feat(client.go): implement client structure

--- exercises/client.go
+++ exercises/client.go
@@ -1,30 +1,62 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
-func createMessage() (json_data map[string]string) {
-	json_data = make(map[string]string)
-	json_data["key1"] = "value1"
-	json_data["key2"] = "value2"
+type Message struct {
+	content string
+}
+
+func createMessage() (m Message) {
+	m = Message{"Message content!"}
 
 	return
 }
 
-func main() {
-	response, err := http.Post(ENDPOINT, "application/json", bytes.NewBuffer(createMessage()))
+func createRequest(m Message) (req *http.Request) {
+	endpoint := "http://localhost:1997"
+	buf, err := json.Marshal(m.content)
 	if err != nil {
-	todo: handle?
-	panic(err)
+		panic(err)
+	}
+
+	req, err = http.NewRequest("POST", endpoint, bytes.NewBuffer(buf))
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	return
+}
+
+func useBody(body []byte) {
+	// todo: do something with body
+	fmt.Print(string(body))
+}
+
+func main() {
+	client := &http.Client{}
+	m := createMessage()
+	req := createRequest(m)
+
+	response, err := client.Do(req)
+	if err != nil {
+		//todo: handle?
+		panic(err)
 	}
 	defer response.Body.Close()
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-	  // todo: handle?
-	  panic(err)
+		// todo: handle?
+		panic(err)
 	}
 
-	doSomethingWithBody(body)
+	useBody(body)
 }
