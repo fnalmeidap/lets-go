075c6c40d7b50d996bf0ae636839a3fa1ab8ae8b
Felipe Nunes <fnap@cin.ufpe.br>
2023-11-22 15:02:44 -0300

    wip(client.go): create message as dict

--- exercises/client.go
+++ exercises/client.go
@@ -1,28 +1,30 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "io/ioutil"
+	"fmt"
 )
 
-func createMessage() {
-  // todo: do something
+func createMessage() (json_data map[string]string) {
+	json_data = make(map[string]string)
+	json_data["key1"] = "value1"
+	json_data["key2"] = "value2"
+
+	return
 }
 
 func main() {
-  response, err := http.Get(ENDPOINT)
-  if err != nil {
-    // todo: handle?
-    panic(err)
-  }
-  defer response.Body.Close()
+	response, err := http.Post(ENDPOINT, "application/json", bytes.NewBuffer(createMessage()))
+	if err != nil {
+	todo: handle?
+	panic(err)
+	}
+	defer response.Body.Close()
 
-  body, err := ioutil.ReadAll(response.Body)
-  if err != nil {
-    // todo: handle?
-    panic(err)
-  }
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+	  // todo: handle?
+	  panic(err)
+	}
 
-  doSomethingWithBody(body)
+	doSomethingWithBody(body)
 }
