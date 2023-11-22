1c3b75cff7447f6750085c55b23ebfbeb75e62a8
Felipe Nunes <fnap@cin.ufpe.br>
2023-11-22 16:21:28 -0300

    feat(server.go): implement server structure

--- exercises/server.go
+++ exercises/server.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 const SERVER_PORT string = ":1997"
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	// do something
+func handlePostRequest(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// todo: processar body
+	fmt.Print("Received a POST request:\n")
+	fmt.Print(string(body))
+
+	w.Write([]byte("Message received!"))
+}
+
+func serverImpl(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		handlePostRequest(w, r)
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
+		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handleRequest)
+	http.HandleFunc("/", serverImpl)
 
+	fmt.Println("Starting server on http://localhost" + SERVER_PORT)
 	err := http.ListenAndServe(SERVER_PORT, nil)
 	if err != nil {
 		panic(err)
