f471024b0d4efc8703ecf93bf304b6b6ae28767b
Felipe Nunes <fnap@cin.ufpe.br>
2023-12-09 17:26:43 -0300

    refactor(exercise_01): separate implementations and add docs

--- exercise_01/experiment.sh
+++ exercise_01/experiment.sh
@@ -2,7 +2,7 @@
 
 for (( i=0; i<30; i++ ))
 do
-    go run server_sequential.go &
+    go run server_sequential_v1.go &
     sleep 0.1
     go run client.go &
 
@@ -13,7 +13,7 @@ echo "================="
 
 for (( i=0; i<30; i++ ))
 do
-    go run server_concurrent.go &
+    go run server_concurrent_v1.go &
     sleep 0.1
     go run client.go &
     wait
